1efefa9b09e58a1007e804ad0c5991c54b2caa9d
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-05 12:40:08 +0100

    added more styling and changes in html tree

--- src/component/NewsLetter.jsx
+++ src/component/NewsLetter.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import NewsLetterForm from './hooks/NewsLetterForm.jsx'
 const NewsLetter = () => {
-  return <div>NewsLetter</div>
+  return (
+    <>
+      <div className="flex flex-col text-center items-center justify-evenly h-[100vh] w-[100vw] relative">
+        <div>
+          <NewsLetterForm />
+        </div>
+        <div className=" absolute bottom-4">
+          <h1>all rights are reserved</h1>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default NewsLetter
---
+++ src/component/hooks/NewsLetterForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewsLetterForm = () => {
+  return <div>NewsLetterForm</div>
+}
+
+export default NewsLetterForm
