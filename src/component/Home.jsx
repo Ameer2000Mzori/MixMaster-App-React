58cfdf591b5023596ec38433919d0458232c509f
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-05 12:31:38 +0100

    added styling and more

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react'
 const Home = () => {
   return (
     <>
-      <h1>this is home page</h1>
+      <div className="h-[100vh] w-[100vw] text-center items-center justify-evenly flex flex-col">
+        <div className="flex flex-col text-center items-center justify-center gap-4 w-[300px] h-[150px] bg-zinc-500 rounded-md text-white">
+          <h1>search something</h1>
+          <div className=" flex flex-row text-center items-center justify-center gap-4">
+            <input type="text" className="text-black text-start pl-4" />
+            <button>Search</button>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </>
   )
 }
