8152a2c4caafda004f41d71ad53a2d84909a4ce7
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-05 11:51:13 +0100

    added query client provider and new query client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
